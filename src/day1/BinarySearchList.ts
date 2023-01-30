aa7c9dc7c5f7bebc672e0d9931e91c7a7e86e04b
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-01-30 19:34:19 +0100

    implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+  if (haystack.length <= 1) {
+    if (haystack[0] === needle) return true;
+    else return false;
+  }
+  const middle = Math.floor(haystack.length / 2);
+  if (needle === haystack[middle]) {
+    return true;
+  }
+  if (needle > haystack[middle]) {
+    return bs_list(haystack.slice(middle + 1), needle);
+  }
+  else {
+    return bs_list(haystack.slice(0, middle), needle);
+  }
 }
