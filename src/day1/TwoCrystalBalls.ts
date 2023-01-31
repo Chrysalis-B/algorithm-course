dd6e9772f1343de9c77d957514b6d55394505cf2
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-01-31 19:43:09 +0100

    implement two crystal balls exercise

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-
+  let low = 0;
+  let high = Math.floor(Math.sqrt(breaks.length));
+  do {
+    if (breaks[high]) {
+      for (let i = low; i <= high; i++) {
+        if (breaks[i]) return i;
+      }
+    }
+    else {
+      low = high;
+      high = high + Math.floor(Math.sqrt(breaks.length));
+    }
+  } while (high < breaks.length);
+  return -1;
 }
