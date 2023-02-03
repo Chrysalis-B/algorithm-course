984ae9ce624499ef4580ae154a178f00b98a20e1
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-03 18:11:45 +0100

    implement queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,44 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item, next: undefined } as Node<T>;
+    this.length++;
+    if (!this.tail) {
+      this.tail = this.head = node;
+      return;
     }
+    this.tail.next = node;
+    this.tail = node;
+  }
 
-    enqueue(item: T): void {
+  deque(): T | undefined {
+    if (!this.head) {
+      return;
+    }
+    this.length--;
+    const oldHead = this.head;
+    this.head = this.head.next;
+    oldHead.next = undefined;
+    if (this.length === 0) {
+      this.tail = undefined;
+    }
+    return oldHead.value;
+  }
 
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
