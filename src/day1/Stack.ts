339985aabff598c7121594b021e0b19b4f4ef730
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-08 16:24:44 +0100

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,37 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class Stack<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
 
-    
+  constructor() {
+    this.head = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  push(item: T): void {
+    if (!this.head) {
+      this.head = { value: item };
     }
-
-    push(item: T): void {
-
-}
-    pop(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
+    else {
+      const currentHead = this.head;
+      this.head = { value: item, next: currentHead };
+    }
+    this.length++;
+  }
+  pop(): T | undefined {
+    if (!this.head) {
+      return;
+    }
+    const currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    return currentHead.value;
+  }
+  peek(): T | undefined {
+    return this.head?.value;
+  }
 }
