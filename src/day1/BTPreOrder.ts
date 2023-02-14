65fa1abf8ce2b1fd0b790097c11b56d8d9c791b8
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-14 18:58:26 +0100

    implement Binary tree pre order traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,11 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
 
+const walk = (curr: BinaryNode<number>, path: number[]): number[] => {
+  if (!curr) return path;
+  path.push(curr.value);
+  if (curr.left) walk(curr.left, path);
+  if (curr.right) walk(curr.right, path);
+  return path;
 }
