64e1282bea750512a8df3b91e34ffde8fdc38e39
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-14 19:08:24 +0100

    improve pre and in order binary tree traversals

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -3,10 +3,10 @@ export default function in_order_search(head: BinaryNode<number>): number[] {
 }
 
 
-const walk = (curr: BinaryNode<number>, path: number[]): number[] => {
+const walk = (curr: BinaryNode<number> | null, path: number[]): number[] => {
   if (!curr) return path;
-  if (curr.left) walk(curr.left, path);
+  walk(curr.left, path);
   path.push(curr.value);
-  if (curr.right) walk(curr.right, path);
+  walk(curr.right, path);
   return path;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -2,10 +2,10 @@ export default function pre_order_search(head: BinaryNode<number>): number[] {
   return walk(head, []);
 }
 
-const walk = (curr: BinaryNode<number>, path: number[]): number[] => {
+const walk = (curr: BinaryNode<number> | null, path: number[]): number[] => {
   if (!curr) return path;
   path.push(curr.value);
-  if (curr.left) walk(curr.left, path);
-  if (curr.right) walk(curr.right, path);
+  walk(curr.left, path);
+  walk(curr.right, path);
   return path;
 }
