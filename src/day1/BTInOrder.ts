f58368abc869e68583d160d5d317210f4b1eab0e
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-14 18:58:02 +0100

    implement Binary tree in order traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,12 @@
 export default function in_order_search(head: BinaryNode<number>): number[] {
+  return walk(head, []);
+}
 
+
+const walk = (curr: BinaryNode<number>, path: number[]): number[] => {
+  if (!curr) return path;
+  if (curr.left) walk(curr.left, path);
+  path.push(curr.value);
+  if (curr.right) walk(curr.right, path);
+  return path;
 }
