6a3f48c87894f1141203967e4a8b778c46d48c46
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-03-09 16:13:47 +0100

    implement breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  return traverse([head], needle);
+}
 
+
+const traverse = (queue: BinaryNode<number>[], needle: number): boolean => {
+  if (queue.length === 0) {
+    return false;
+  }
+  else if (queue[0].value === needle) {
+    return true;
+  }
+  queue[0].left && queue.push(queue[0].left);
+  queue[0].right && queue.push(queue[0].right);
+  queue.shift();
+  return traverse(queue, needle);
 }
