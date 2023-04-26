45197b143e76df1124a1b5a63f32eab023d8e944
Heini Ahokas <ahokas.heini@gmail.com>
2023-04-26 14:31:53 +0300

    feat: implement depth-first search on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,14 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+export default function dfs(head: BinaryNode<number> | null, needle: number): boolean {
+    if (!head) {
+        return false;
+    }
+    else if (needle === head.value) {
+        return true;
+    }
+    else if (needle <= head.value) {
+        return dfs(head.left, needle);
+    }
+    else {
+        return dfs(head.right, needle);
+    }
 }
